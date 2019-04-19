949a3220e23795cc12094e14763c04699c53587b
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-18 21:26:13 -0400

    test main route

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const Users = require('./../users/usersModel');
 server.use(express.json());
 
 server.get('/', async (req, res) => {
+  res.status(200).json({message: 'Home Page'});
+});
+
+server.get('/users', async (req, res) => {
   const users = await Users.fetchAll();
   res.status(200).json(users);
 });
--- api/server.spec.js
+++ api/server.spec.js
@@ -7,6 +7,7 @@ describe('the server', () => {
     beforeEach(() => {
         return db('users').truncate()
     })
+
     describe('the environment', () => {
         it('should set setting for the environment', () => {
             const env = process.env.DB_ENV
@@ -14,4 +15,20 @@ describe('the server', () => {
             expect(env).toBe('testing')
         })
     })
+
+    describe('GET /', () => {
+        it('should return status 200', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        })
+        it('should return json', async () => {
+            const res = await request(server).get('/');
+            expect(res.type).toBe('application/json');
+        })
+        it('should return { message: "Home Page"}', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({ message: "Home Page"});
+        })
+    })
+
 })

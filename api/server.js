bc847e82da9e3847dcb20490467c0f17934a2ac4
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-18 20:43:14 -0400

    add json users response to / route, seed data

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
+const Users = require('./../users/usersModel');
 server.use(express.json());
 
 server.get('/', async (req, res) => {
-  res.status(200).json({ api: 'up' });
+  const users = await Users.fetchAll();
+  res.status(200).json(users);
 });
 
 module.exports = server;
---
+++ seeds/01-users.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries and resets ids
+    return knex('users')
+      .truncate()
+      .then(function() {
+        return knex('users').insert([
+          { name: 'sam' },
+          { name: 'frodo' },
+          { name: 'pippin' },
+          { name: 'merry' },
+        ]);
+      });
+  };
+  
